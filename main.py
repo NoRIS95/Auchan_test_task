6db356e43a0076348b99ea286b88b4aea920d856
ivan <ryzhikov95@gmail.com>
2024-06-28 20:35:59 +0300

    Сode is written

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+
+dirs = ['TEST_Folder_1', 'TEST_Folder_2']
+REC_DIR = 'Result'
+
+
+def rec_file(dir, text_file):
+    num_file = (text_file[text_file.find("N_") + 1 : text_file.find(".txt")])
+    f = open(f'{dir}/{text_file}', 'r')
+    nums = []
+    for string in f:
+        string = string.replace('"', '')
+    for sym in string.split(','):
+        if '-' not in sym:
+            num = int(sym)
+            nums.append(num)
+        else:
+            start_finish_nums = sym.split('-')
+            for num in range(int(start_finish_nums[0]), int(start_finish_nums[1]) + 1):
+                nums.append(num)
+    if not os.path.exists(REC_DIR):
+        os.mkdir(REC_DIR)
+    with open(f'{REC_DIR}/TEST_AUCHAN_success{num_file}.txt', 'a') as file:
+        for i in sorted(set(nums)):
+            file.write(str(i) + '\n')
+    return
+
+
+for dir in dirs:
+    file_for_dir = os.listdir(dir)
+    for text_file in file_for_dir:
+        rec_file(dir, text_file)
+
