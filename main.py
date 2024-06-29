66e20a4b7e91ce295fb54cbf934184873a32e143
ivan <ryzhikov95@gmail.com>
2024-06-29 15:00:50 +0300

    Functional decomposition is completed

--- main.py
+++ main.py
@@ -4,28 +4,42 @@ import glob
 
 
 RESULTS_DIR = 'Result'
+FIRST_PART_NEW_NAME = 'TEST_AUCHAN_success'
+
+
+def read_file(file_path, dir_path):
+    file_info = {}
+    with open(file_path, 'r') as f:
+        file_name = file_path.replace(f'{dir_path}/', '')
+        for line in f:
+            string = line.replace('"', '')
+        file_info['File_name'] = file_name
+        file_info['String'] = string
+    return file_info
+
+
+
+def write_file(dir, file_name, nums):
+    with open(f'{RESULTS_DIR}/{FIRST_PART_NEW_NAME}_{dir}_{file_name}', 'w') as file:
+        for i in nums:
+            file.write(str(i) + '\n')
 
 
 def process_file(dir, dir_path, file_path):
-    with open(file_path, 'r') as f:
-        file_name = file_path.replace(f'{dir_path}/', '')
-        nums = []
-        for string in f:
-            string = string.replace('"', '')
-        for sym in string.split(','):
-            if '-' not in sym:
-                num = int(sym)
+    file_info = read_file(file_path, dir_path)
+    nums = []
+    for sym in file_info['String'].split(','):
+        if '-' not in sym:
+            num = int(sym)
+            nums.append(num)
+        else:
+            start_finish_nums = sym.split('-')
+            for num in range(int(start_finish_nums[0]), int(start_finish_nums[1]) + 1):
                 nums.append(num)
-            else:
-                start_finish_nums = sym.split('-')
-                for num in range(int(start_finish_nums[0]), int(start_finish_nums[1]) + 1):
-                    nums.append(num)
-        if not os.path.exists(RESULTS_DIR):
-            os.mkdir(RESULTS_DIR)
-        with open(f'{RESULTS_DIR}/TEST_AUCHAN_success_{dir}_{file_name}', 'w') as file:
-            for i in sorted(set(nums)):
-                file.write(str(i) + '\n')
-    return
+    if not os.path.exists(RESULTS_DIR):
+        os.mkdir(RESULTS_DIR)
+    sorted_nums = sorted(set(nums))
+    write_file(dir, file_info['File_name'], sorted_nums)
 
 
 if __name__ == '__main__':
