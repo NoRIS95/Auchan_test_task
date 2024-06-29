1455d6128609cc09a3c26fe677d62e1ad8ad7b5c
ivan <ryzhikov95@gmail.com>
2024-06-29 14:17:36 +0300

    Output files are now saved under different names

--- main.py
+++ main.py
@@ -1,45 +1,46 @@
 import os
 import logging
-
-dirs = os.listdir()
-dirs.remove('main.py')
-dirs.remove('README.md')
-dirs.remove('.git')
-REC_DIR = 'Result'
+import glob
 
 
-def rec_file(dir, text_file):
-    num_file = (text_file[text_file.find("N_") + 1 : text_file.find(".txt")])
-    num_file = num_file.replace('_', '')
-    f = open(f'{dir}/{text_file}', 'r')
-    nums = []
-    for string in f:
-        string = string.replace('"', '')
-    for sym in string.split(','):
-        if '-' not in sym:
-            num = int(sym)
-            nums.append(num)
-        else:
-            start_finish_nums = sym.split('-')
-            for num in range(int(start_finish_nums[0]), int(start_finish_nums[1]) + 1):
+RESULTS_DIR = 'Result'
+
+
+def process_file(dir, dir_path, file_path):
+    with open(file_path, 'r') as f:
+        file_name = file_path.replace(f'{dir_path}/', '')
+        nums = []
+        for string in f:
+            string = string.replace('"', '')
+        for sym in string.split(','):
+            if '-' not in sym:
+                num = int(sym)
                 nums.append(num)
-    if not os.path.exists(REC_DIR):
-        os.mkdir(REC_DIR)
-    with open(f'{REC_DIR}/TEST_AUCHAN_success{num_file}.txt', 'a') as file:
-        for i in sorted(set(nums)):
-            file.write(str(i) + '\n')
+            else:
+                start_finish_nums = sym.split('-')
+                for num in range(int(start_finish_nums[0]), int(start_finish_nums[1]) + 1):
+                    nums.append(num)
+        if not os.path.exists(RESULTS_DIR):
+            os.mkdir(RESULTS_DIR)
+        with open(f'{RESULTS_DIR}/TEST_AUCHAN_success_{dir}_{file_name}', 'w') as file:
+            for i in sorted(set(nums)):
+                file.write(str(i) + '\n')
     return
 
 
 if __name__ == '__main__':
+    dirs = os.listdir()
+    dirs.remove('main.py')
+    dirs.remove('README.md')
+    dirs.remove('.git')
     for dir in dirs:
-        if dir is not REC_DIR:
+        if dir != RESULTS_DIR:
             if not os.path.exists(dir):
                 logging.error(f"Directory {dir} is not found")
                 continue
-            files_for_dir = os.listdir(dir)
-            if len(files_for_dir) == 0:
+            dir_path = f'{os.getcwd()}/{dir}'
+            file_pathes_for_dir = glob.glob(dir_path + '/TEST_*')
+            if len(file_pathes_for_dir) == 0:
                 logging.warning(f"Directory {dir} is empty")
-            for text_file in files_for_dir:
-                rec_file(dir, text_file)
-
+            for file_path in file_pathes_for_dir:
+                process_file(dir, dir_path, file_path)
